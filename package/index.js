5356d47db7e55cd39186903743fc55798e65f007
Adi Sahar <adisahar1@hotmail.com>
2019-09-05 02:19:58 +0300

    refactor to neutrino

--- package/index.js
+++ package/index.js
@@ -6,38 +6,57 @@
 
 const minimist = require("minimist");
 const { spawn } = require("child_process");
-const {
-  preflight,
-  logSignature,
-  normalizeScript
-} = require("./config/helpers");
+const { Neutrino, build } = require("neutrino");
 
-// Normalize our scripts to be react-scripts compatible,
-// e.g.: `test:production` will be mapped to `test`, and a coverage
-// flag will be set to `true` for adding a coverage report in production build.
-const argv = process.argv;
-const args = minimist(argv.slice(2));
-const rawScript = args._[0];
-const scriptArgs = [].concat(normalizeScript({ rawScript }));
-const restArgs = argv.slice(3);
-const allArgs = [...scriptArgs, ...restArgs];
+const neutrino = Neutrino();
 
-const silentLogger = { log: () => {}, error: () => {} };
-
-if (
-  preflight({
-    script: rawScript,
-    logger: args.inspect ? silentLogger : undefined
+neutrino
+  .register("build", build)
+  .use("@neutrinojs/react", {
+    style: {
+      test: /\.sass$/,
+      modulesTest: /\.module\.sass$/,
+      loaders: [{ loader: require.resolve("sass-loader"), useId: "sass" }]
+    }
   })
-) {
-  const runScript = spawn("node", allArgs, {
-    stdio: "inherit",
-    env: { PARENT_ARGV: JSON.stringify(process.argv) }
-  });
+  .run("build")
+  .fork(
+    errors => errors.forEach(console.error),
+    stats => console.log(stats.toString({ colors: true }))
+  );
 
-  runScript.on("error", err => process.exit(err));
-  runScript.on("close", code => {
-    !args.inspect && logSignature();
-    process.exit(code);
-  });
-}
+// const {
+//   preflight,
+//   logSignature,
+//   normalizeScript
+// } = require("./config/helpers");
+
+// // Normalize our scripts to be react-scripts compatible,
+// // e.g.: `test:production` will be mapped to `test`, and a coverage
+// // flag will be set to `true` for adding a coverage report in production build.
+// const argv = process.argv;
+// const args = minimist(argv.slice(2));
+// const rawScript = args._[0];
+// const scriptArgs = [].concat(normalizeScript({ rawScript }));
+// const restArgs = argv.slice(3);
+// const allArgs = [...scriptArgs, ...restArgs];
+
+// const silentLogger = { log: () => {}, error: () => {} };
+
+// if (
+//   preflight({
+//     script: rawScript,
+//     logger: args.inspect ? silentLogger : undefined
+//   })
+// ) {
+//   const runScript = spawn("node", allArgs, {
+//     stdio: "inherit",
+//     env: { PARENT_ARGV: JSON.stringify(process.argv) }
+//   });
+
+//   runScript.on("error", err => process.exit(err));
+//   runScript.on("close", code => {
+//     !args.inspect && logSignature();
+//     process.exit(code);
+//   });
+// }
